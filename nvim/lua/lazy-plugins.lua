2e26f8259dca72b56873492f0797017982887c3b
lemon14454 <lemon14454@gmail.com>
2024-05-10 23:38:03 +0800

    Setup harpoon and remove neo-tree

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -7,7 +7,6 @@ require('lazy').setup {
   require 'custom.plugins.lint',
   require 'custom.plugins.lsp',
   require 'custom.plugins.mini',
-  require 'custom.plugins.neo-tree',
   require 'custom.plugins.nvim-cmp',
   require 'custom.plugins.telescope',
   require 'custom.plugins.treesitter',
@@ -16,6 +15,11 @@ require('lazy').setup {
   'tpope/vim-rhubarb',
   'tpope/vim-sleuth',
   { 'numToStr/Comment.nvim', opts = {} },
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
 
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 }
