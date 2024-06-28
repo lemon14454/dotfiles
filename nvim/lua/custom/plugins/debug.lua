a8b62ae80d66d0df45d2c76d37dfa17ff7ffe60a
lemon14454 <lemon14454@gmail.com>
2024-06-28 13:29:40 +0800

    setup nvim dap for dotnet

--- nvim/lua/custom/plugins/debug.lua
+++ nvim/lua/custom/plugins/debug.lua
@@ -1,18 +1,18 @@
 return {
   {
-    "mfussenegger/nvim-dap",
+    'mfussenegger/nvim-dap',
     dependencies = {
-      "leoluz/nvim-dap-go",
-      "rcarriga/nvim-dap-ui",
-      "nvim-neotest/nvim-nio",
-      "williamboman/mason.nvim",
+      'leoluz/nvim-dap-go',
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+      'williamboman/mason.nvim',
     },
     config = function()
-      local dap = require "dap"
-      local ui = require "dapui"
+      local dap = require 'dap'
+      local ui = require 'dapui'
 
-      require("dapui").setup()
-      require("dap-go").setup()
+      require('dapui').setup()
+      require('dap-go').setup()
 
       -- Handled by nvim-dap-go
       -- dap.adapters.go = {
@@ -24,20 +24,36 @@ return {
       --   },
       -- }
 
-      vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
-      vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+      -- C#
+      dap.adapters.coreclr = {
+        type = 'executable',
+        command = '/usr/local/netcoredbg',
+        args = { '--interpreter=vscode' },
+      }
+
+      dap.configurations.cs = {
+        {
+          type = 'coreclr',
+          name = 'launch - netcoredbg',
+          request = 'launch',
+          program = function()
+            return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+          end,
+        },
+      }
+
+      vim.keymap.set('n', '<space>b', dap.toggle_breakpoint)
+      vim.keymap.set('n', '<space>gb', dap.run_to_cursor)
 
       -- Eval var under cursor
-      vim.keymap.set("n", "<space>?", function()
-        require("dapui").eval(nil, { enter = true })
+      vim.keymap.set('n', '<space>?', function()
+        require('dapui').eval(nil, { enter = true })
       end)
 
-      vim.keymap.set("n", "<F1>", dap.continue)
-      vim.keymap.set("n", "<F2>", dap.step_into)
-      vim.keymap.set("n", "<F3>", dap.step_over)
-      vim.keymap.set("n", "<F4>", dap.step_out)
-      vim.keymap.set("n", "<F5>", dap.step_back)
-      vim.keymap.set("n", "<F13>", dap.restart)
+      vim.keymap.set('n', '<F5>', dap.continue)
+      vim.keymap.set('n', '<F6>', dap.step_over)
+      vim.keymap.set('n', '<F7>', dap.step_into)
+      vim.keymap.set('n', '<F8>', dap.step_out)
 
       dap.listeners.before.attach.dapui_config = function()
         ui.open()
--- nvim/lua/custom/plugins/lsp.lua
+++ nvim/lua/custom/plugins/lsp.lua
@@ -98,6 +98,7 @@ return { -- LSP Configuration & Plugins
       -- But for many setups, the LSP (`tsserver`) will work just fine
       tsserver = {},
       --
+      omnisharp = {},
 
       lua_ls = {
         -- cmd = {...},
